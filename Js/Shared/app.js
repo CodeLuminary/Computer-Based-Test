5bcf8fd7a4c6ecd76fc83acb9695371da4f5765b
CodeLuminary <victorijoni@yahoo.com>
2021-10-08 14:20:23 +0100

    Sample questions added

--- Js/Shared/app.js
+++ Js/Shared/app.js
@@ -1,4 +1,31 @@
-var examLength = 61; //Exam length in minutes
+//This is an example of a json data
+var exam = {
+    "duration": 61,
+    "instruction": "Please answer all questions and do not waste time with any question because some are harder than the others",
+    "questions":[
+        {
+            "question": "Who is the current president of the United states",
+            "options": ["Muhammedu Buhari","Vlamir Putin", "Joe Biden"],
+            "answerPosition": 3
+        },
+        {
+            "question": "How many countries are in the world?",
+            "options":[249,195,300],
+            "answerPosition": 1
+        },
+        {
+            "question": "How many continents are in the world?",
+            "options": [7, 5, 10, 8, 15],
+            "answerPosition": 0
+        },
+        {
+            "question": "How many countries are in the united nations?",
+            "options": [120, 190, 193,200],
+            "answerPosition": 2
+        }
+    ]
+}
+var examLength = exam.duration; //Exam length in minutes
 var sec = 0;
 var timerObject;
 function showCbt(){
